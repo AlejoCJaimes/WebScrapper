3daf715a3d54d68864455a9a7b9159873e3122e0
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-11-06 20:27:02 -0500

    Actualizacion de credenciales, se destituyeron por clase y se envían por lista desde el file main.py

--- project/ETL/job_ext_rnvb_vnyh.py
+++ project/ETL/job_ext_rnvb_vnyh.py
@@ -1,31 +1,32 @@
 import pandas as pd
 from sodapy import Socrata
-import credentials as cred
-
+#import credentials
 
 class Extract:
     def __init__(self):
         self._dataset_datos_gov = None
+        #self._credentials = credentials.Credentials().ext()
 
-    def extract(self):
+    def extract(self,app_token_, username_, password_):
         # authenticated client
         client = Socrata(domain="www.datos.gov.co",
-                 app_token=cred.data.getToken(),
-                 username=cred.data.getUser(),
-                 password=cred.data.getPass())
+                 app_token=app_token_,
+                 username=username_,
+                 password=password_)
+                #  app_token=self._credentials[0],
+                #  username=self._credentials[1],
+                #  password=self._credentials[2])
 
-        results = client.get("rnvb-vnyh", limit=2000) # Dataset_S_OK
+        results = client.get("rnvb-vnyh", limit=3000) # Dataset_S_OK
 
         # Convert to pandas DataFrame
         self._dataset_datos_gov = pd.DataFrame.from_records(results)
-    
-    def run_extract(self):
-        self.extract()
-        
-    def load_dataset(self):
         return self._dataset_datos_gov
-data_set = Extract()
-data_set.run_extract()
-data_set.load_dataset()
-
+    # @property
+    # def run_extract(self):
+    #     self.extract()
+    #     return self.load_dataset()
+        
+    # def load_dataset(self):
+    #     return self._dataset_datos_gov
     
