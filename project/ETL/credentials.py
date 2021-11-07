91b2d4189d3ee3a2c4a474a7ef338af832cabfec
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-11-06 20:21:36 -0500

    Actualizacion de enrutamiento y estructura de llamado para las credenciales

--- project/ETL/credentials.py
+++ project/ETL/credentials.py
@@ -4,40 +4,22 @@ class Credentials:
         self._passwd = None
         self._app_token = None
 
-    def setUser(self, username):
-        self._username = username
-
-    def setPass(self, passd):
-        self._passwd = passd
-
-    def setAppToken(self, app_token):
-        self._app_token = app_token
-
-    def getUser(self):
-        return self._username
-
-    def getPass(self):
-        return self._passwd
-
-    def getToken(self):
-        return self._app_token
-    username = property(getUser, setUser)
-    passwd = property(getPass, setPass)
-    app_token = property(getToken, setAppToken)
-
-
-def ext():
-    with open('/home/alev/personalProjects/WebScrapper/project/Acc.txt', 'r') as f:
-        data = f.read()
-    res = [x.strip() for x in data.split(',')]
-    return res
+    def ext(self):
+        with open('/home/alev/personalProjects/WebScrapper/project/Acc.txt', 'r') as f:
+            data = f.read()
+        res = [x.strip() for x in data.split(',')]
+        self._username = res[0]
+        self._passwd = res[1]
+        self._app_token = res[2]
+        return self._app_token, self._username, self._passwd
+    
 
 
 
-cred = ext()
-data = Credentials()
-data.setUser(cred[0])
-data.setPass(cred[1])
-data.setAppToken(cred[2])
+# cred = ext()
+# data = Credentials()
+# data.setUser(cred[0])
+# data.setPass(cred[1])
+# data.setAppToken(cred[2])
     # u.setUser(list(cred.items())[0][0])
     # u.setPass(list(cred.items())[0][1])
