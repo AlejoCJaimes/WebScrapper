1e9d81d197bcaa9cf8563c3bedbc99a10b728257
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-11-06 20:23:58 -0500

    Archivo de manejo, para los Data Pages Objects, devuelve el JSOn de etiquetas

--- project/web_scrapper_labels/data_page_objects.py
+++ project/web_scrapper_labels/data_page_objects.py
@@ -1,29 +1,41 @@
 # Representa la página principal de la web
 import requests
 import bs4
+import json
+import os
 from common import config
+from vap_route import ROOT_RUOUTE
+from vap_route import PROJECT_ROUTE
 
 
 class HomePage:
     def __init__(self, data_site_uid, url):
         self._config = config()['data_sites'][data_site_uid]
-        self._queries = self._config['queries']
+        self._sodaQueries = '$limit=1'
         self._html = None
+        self._path = None
+        self._path_relative_project = None
+        self._name_file = None
         self._visit(url)
 
     #generate propertys -> page object pattern
     @property
     def page_labels(self):
-        label_list = []
-        for label in self._select(self._queries['homepage_page_labels']):
-            if label and label.has_attr('href'):
-                label_list.append(label)
-        return set(label['href'] for label in label_list)
-
-    def select(self, query_string):
-        return self._html.select(query_string)
+        return self._html
+    
+    @property
+    def save_json_labels(self):
+        self._name_file = 'unstructured_labels.json'
+        self._path = ROOT_RUOUTE() + PROJECT_ROUTE() + self._name_file
+        self._path_relative_project = PROJECT_ROUTE()
+        with open(self._path, 'w') as json_file:
+            json.dump(self._html.text,json_file)
+        return self._path
+    
     
     def _visit(self, url):
-        response = requests.get(url)
+        response = requests.get(url + self._sodaQueries)
         response.raise_for_status() # levantar un error, si no sucedió correctamente
         self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+        
+    
