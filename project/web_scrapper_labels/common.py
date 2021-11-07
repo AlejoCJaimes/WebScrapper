e43ec659695308184673ba0fdd45cc550f0f6657
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-11-06 20:19:46 -0500

    Actualizacion de enrutamiento

--- project/web_scrapper_labels/common.py
+++ project/web_scrapper_labels/common.py
@@ -1,11 +1,11 @@
 import yaml
-
+from vap_route import CONFIG_ROUTE
 __config = None #Cachea nuestrar configuracion
 
 def config():
     global __config
     if not __config:
-         with open('/home/alev/personalProjects/WebScrapper/project/web_scrapper_labels/config.yaml',mode = 'r') as f:
+         with open(CONFIG_ROUTE(),mode = 'r') as f:
              __config =yaml.safe_load(f)
 
     return __config
