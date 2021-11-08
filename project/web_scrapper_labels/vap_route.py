1b4db30e1fa99101e85213294ba20b7d15e5b06c
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-11-07 21:56:03 -0500

    Actualizacion de formateo de código

--- project/web_scrapper_labels/vap_route.py
+++ project/web_scrapper_labels/vap_route.py
@@ -5,6 +5,8 @@ __ROOT_RUOUTE = None
 __ETL_ROUTE = None
 __CREDENTIALS_ROUTE = None
 __CONFIG_ROUTE = None
+
+
 def ROOT_RUOUTE():
     global __ROOT_RUOUTE
     root_route = (os.path.dirname(os.path.realpath(__file__)))
@@ -12,26 +14,30 @@ def ROOT_RUOUTE():
     __ROOT_RUOUTE = "".join(pattern.findall(root_route, 0, 28))
     return __ROOT_RUOUTE
 
+
 def PROJECT_ROUTE():
     global __PROJECT_ROUTE
     root_route = (os.path.dirname(os.path.realpath(__file__)))
     pattern = re.compile(r'(/.+)')
-    __PROJECT_ROUTE = "".join(pattern.findall(root_route,27))
+    __PROJECT_ROUTE = "".join(pattern.findall(root_route, 27))
     return __PROJECT_ROUTE + '/'
+
 
 def ETL_ROUTE():
     global __ETL_ROUTE
     root_route = (os.path.dirname(os.path.realpath(__file__)))
     pattern = re.compile(r'(/.+)')
-    __ETL_ROUTE = "".join(pattern.findall(root_route,27,48))
-    return __ETL_ROUTE+ 'ETL' + '/'
+    __ETL_ROUTE = "".join(pattern.findall(root_route, 27, 48))
+    return __ETL_ROUTE + 'ETL' + '/'
+
 
 def CREDENTIALS_ROUTE():
     global __CREDENTIALS_ROUTE
     root_route = (os.path.dirname(os.path.realpath(__file__)))
     pattern = re.compile(r'(/.+)')
-    __CREDENTIALS_ROUTE = "".join(pattern.findall(root_route,27,48))
+    __CREDENTIALS_ROUTE = "".join(pattern.findall(root_route, 27, 48))
     return __CREDENTIALS_ROUTE + 'Acc.txt'
+
 
 def CONFIG_ROUTE():
     global __CONFIG_ROUTE
