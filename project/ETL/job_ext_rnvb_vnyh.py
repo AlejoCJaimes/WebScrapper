06a20b653f3d3eb25c6ae89b26ef48f6f58c835a
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-11-14 16:43:22 -0500

    Actualizacion en la extracción: 	- Se parametriza el límite para no enviar un valor por defecto.} 	- El valor parametrizado es el total de datos que hay para este dataset en la nube.

--- project/ETL/job_ext_rnvb_vnyh.py
+++ project/ETL/job_ext_rnvb_vnyh.py
@@ -7,26 +7,18 @@ class Extract:
         self._dataset_datos_gov = None
         #self._credentials = credentials.Credentials().ext()
 
-    def extract(self,app_token_, username_, password_, site_):
-        # authenticated client
+    def extract(self,app_token_, username_, password_, site_, limit_):
+        #authenticated client
         client = Socrata(domain="www.datos.gov.co",
                  app_token=app_token_,
                  username=username_,
                  password=password_)
-                #  app_token=self._credentials[0],
-                #  username=self._credentials[1],
-                #  password=self._credentials[2])
+            
 
-        results = client.get(site_,limit=504872) # Dataset_S_OK
+        results = client.get(site_,limit=limit_) # Dataset_S_OK
 
         # Convert to pandas DataFrame
         self._dataset_datos_gov = pd.DataFrame.from_records(results)
         return self._dataset_datos_gov
-    # @property
-    # def run_extract(self):
-    #     self.extract()
-    #     return self.load_dataset()
-        
-    # def load_dataset(self):
-    #     return self._dataset_datos_gov
+ 
     
