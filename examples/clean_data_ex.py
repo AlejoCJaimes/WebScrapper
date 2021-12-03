e1249b11aa709a06df27ac9618b218babd5f5d05
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-12-02 21:39:11 -0500

    Actualizacion de limpiamiento de datos

--- examples/clean_data_ex.py
+++ examples/clean_data_ex.py
@@ -1,9 +1,41 @@
 import pandas as pd
 import numpy as np
+from tabulate import tabulate
+
 
 def load_dataset():
-    ds = pd.read_csv('project/ETL/RawFiles/ds_saber_11_2020_1_191120210656.csv', encoding='utf-8')
-    ls =ds.isnull().sum()
-    return print(ls)
+    ds = pd.read_csv(
+        'project/ETL/RawFiles/ds_saber_11_2020_1_191120210656.csv', encoding='utf-8')
+    return ds
 
-load_dataset()
+
+def load_labels(filename):
+    labels = open(filename+'.txt')
+    return [label for label in "".join(labels.readlines()).split(',')]
+
+
+def missing_data(ds_saber_11_2020):
+    data_nulls_values = [nan for nan in ds_saber_11_2020.isnull().sum()]
+    data_nulls_labels = [nan_l for nan_l in ds_saber_11_2020.columns]
+    nulls_values = dict(zip(data_nulls_labels, data_nulls_values))
+    return nulls_values, ds_saber_11_2020.isnull().sum().sum()
+
+
+def show_missing_data(dict_nulls):
+    nulls_values = dict(zip([k for k, v in dict_nulls[0].items() if v > 0], [
+                        v for k, v in dict_nulls[0].items() if v > 0]))
+    return nulls_values, ['Etiquetas', '#Datos']
+
+
+def null_handling_values(ds):
+    nulls_values = missing_data(ds)
+    print('Se encontraron {} datos vacios o nulos.'.format(
+        nulls_values[1])) if nulls_values[1] > 0 else print('No se encontraron datos vacios.')
+    if nulls_values[1] > 0:
+        info = show_missing_data(nulls_values)
+        print(tabulate(info[0].items(), headers=info[1])) if bool(
+            input('¿Desea ver detalle de datos vacios? n/y').lower() == 'y') else None
+        return info[0]
+    else:
+        pass
+    return 0
