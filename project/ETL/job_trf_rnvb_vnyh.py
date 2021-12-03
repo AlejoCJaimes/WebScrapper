3f9cd3e1e0bb018761f276b1b0a139b5de41ba71
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-12-03 00:05:47 -0500

    Actualización de Tranformación:  - Se crean los métodos missing_data, show_missing_data, null_handling_values  - se encargan de realizaar todo el tratamiento de los datos del dataset y limpiarlo. Los datos que son considerados como nulos o vacios representan, son reemplazados por un 0.

--- project/ETL/job_trf_rnvb_vnyh.py
+++ project/ETL/job_trf_rnvb_vnyh.py
@@ -1,43 +1,67 @@
-'''Reglas del Negocio
-1. Extraer 2000 datos
-2. Mostrar el número de estudiantes, con tarjeta de identidad
-que presentaron el icfes en zonas rurales y zonas urbanas.
-'''
-
+import sys
 import pandas as pd
-import numpy as np
+from tabulate import tabulate
+sys.path.append('..')
+from web_scrapper_labels.vap_route import ROOT_RUOUTE
+from web_scrapper_labels.vap_route import ETL_ROUTE
 
 class Transform:
     def __init__(self, file_name, path_file):
-        self._data_set_load = None
         self._data_set_dirty = None
         self._data_set_clean = None
-        self._path_file = path_file + file_name.strip()
+        self._data_set_process = None
+        self._path_file = path_file + '/' + file_name.strip()
+
+    def load_labels(self,labelname):
+        aux = ROOT_RUOUTE()[:-1]+ ETL_ROUTE() + 'RawFiles/'
+        labelname = labelname.replace('.csv','.txt')
+        labels = open(aux+labelname)
+        return [label for label in "".join(labels.readlines()).split(',')]
 
     def load_dataset(self):
-        self._data_set_load = pd.read_csv(self._path_file, encoding='utf-8')
+        self._data_set_dirty = pd.read_csv(self._path_file, encoding='utf-8')
+        return self._data_set_dirty
 
-    def change_case_to_upper(self):
-        self._data_set_load = self._data_set_load.columns.str.upper()
-    
-    def fix_missing_data(self):
-        pass
+    def missing_data(self):
+        data_nulls_values = [
+            nan for nan in self._data_set_dirty.isnull().sum()]
+        data_nulls_labels = [nan_l for nan_l in self._data_set_dirty.columns]
+        nulls_values = dict(zip(data_nulls_labels, data_nulls_values))
+        self._data_set_process = (
+            nulls_values, self._data_set_dirty.isnull().sum().sum())
+        return self._data_set_process
 
+    def show_missing_data(self):
+        nulls_values = dict(zip([k for k, v in self._data_set_process[0].items() if v > 0], [
+                            v for k, v in self._data_set_process[0].items() if v > 0]))
+        return nulls_values, ['Etiquetas', '#Datos']
 
-    def drop_columns(self, dataset):
-        dataset = dataset.drop(columns=['estu_tipodocumento'])
-        return dataset
+    def null_handling_values(self):
+        print('Se encontraron {} datos vacios o nulos.'.format(
+            self._data_set_process[1])) if self._data_set_process[1] > 0 else print('No se encontraron datos vacios.')
+        if self._data_set_process[1] > 0:
+            info = self.show_missing_data()
+            print(tabulate(info[0].items(), headers=info[1])) if bool(
+                input('¿Desea ver detalle de datos vacios? n/y\t').lower() == 'y') else None
+            return info[0]
 
-    # def print_dataset(self):
-    #     saber_11_ds_trf = pd.DataFrame(self._data_set_extract, columns=[
-    #                                    'estu_tipodocumento', 'estu_nacionalidad', 'estu_genero', 'estu_fechanacimiento', 'periodo'])
-    #     print(self._data_set_extract.columns)
+    def fill_null_values(self):
+        self._data_set_clean = self._data_set_dirty.fillna(0)
+
+    def get_clean_dataset(self):
+        return self._data_set_clean
 
 
 if __name__ == '__main__':
-    df_pss = Transform()
-    df_pss.load_dataset()
-    print(df_pss.getDataset())
+    df_pss = Transform('2','2')
+    x = df_pss.load_labels('labels_saber_11_2020_1_021220212327.csv')
+    
+    #labels_saber_11_2020_1_021220212327.txt
+    print(x)
+    #labels_saber_11_2020_1_021220212327
+    # ds_saber_11_2020_1_191120210656.csv
+    # labels_saber_11_2020_1_191120210656.txt
+
     # operations with pandas
     #dataset = df_pss.drop_columns(dataset)
     # f_pss.print_dataset()
