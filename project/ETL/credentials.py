10d5562a360b995ba6f3eceadd694f854d6e975d
Alejandro Jaimes <aca.alejandrocjaimes31@gmail.com>
2021-12-17 00:38:58 -0500

    Actualización de rutas, se realizan variables de entorno para manejar las rutas

--- project/ETL/credentials.py
+++ project/ETL/credentials.py
@@ -1,3 +1,11 @@
+import sys
+import os
+import errno
+sys.path.append('.')
+
+from web_scrapper_labels.vap_route import CREDENTIALS_ROUTE
+from web_scrapper_labels.vap_route import ROOT_RUOUTE
+
 class Credentials:
     def __init__(self):
         self._username = None
@@ -5,21 +13,10 @@ class Credentials:
         self._app_token = None
 
     def ext(self):
-        with open('/home/alev/personalProjects/WebScrapper/project/Acc.txt', 'r') as f:
+        with open(ROOT_RUOUTE()[:-1]+CREDENTIALS_ROUTE(), 'r') as f:
             data = f.read()
         res = [x.strip() for x in data.split(',')]
         self._username = res[0]
         self._passwd = res[1]
         self._app_token = res[2]
         return self._app_token, self._username, self._passwd
-    
-
-
-
-# cred = ext()
-# data = Credentials()
-# data.setUser(cred[0])
-# data.setPass(cred[1])
-# data.setAppToken(cred[2])
-    # u.setUser(list(cred.items())[0][0])
-    # u.setPass(list(cred.items())[0][1])
